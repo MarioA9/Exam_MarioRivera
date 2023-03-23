711bc7aa719ca6a77dc6847f5fff05772f72555e
MarioA9 <mario.rivera@uniat.edu.mx>
2023-03-23 11:16:29 -0600

    first commit

---
+++ Examen_MarioRivera.cpp
@@ -0,0 +1,19 @@
+// Examen_MarioRivera.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int option;
+    cout << "elige una opcion";
+    cin >> option;
+    switch (option)
+    {
+    case 1:
+
+    }
+}
+
